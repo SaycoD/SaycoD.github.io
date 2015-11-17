f3e96c9248f1aaecdcbee74e55889e5e95c1bd09
Marco Moutinho <marcomoutinho@live.ca>
2015-11-17 15:38:55 -0500

    Created website, using creative theme from startbootstrap.com

---
+++ js/script.js
@@ -0,0 +1,15 @@
+﻿(function($) {
+    $(document).ready(function() {
+        var is_mobile = false;
+ 
+        if( $('.player').css('display')=='none') {
+            is_mobile = true;
+        }
+        if (is_mobile == true) {
+            //Conditional script here
+            $('.big-background').addClass('bg-dark');
+        }else{
+            $('.player').mb_YTPlayer();
+        }
+    });
+})(jQuery);
